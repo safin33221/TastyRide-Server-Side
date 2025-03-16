5e14736a32d36433b8c9b6bcd4599ffba5964b74
safin33221 <saifn33221@gmail.com>
2025-03-16 23:00:05 +0600

    Initialize Server setup Completed

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const mongoose = require('mongoose')
+require('dotenv').config()
+const cors = require('cors')
+const app = express()
+const prot = process.env.PORT || 8080
+
+//Middle Ware
+app.use(cors())
+app.use(express.json())
+
+
+
+//Mongoose
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log('mogoose conntected succefully'))
+    .catch((err) => console.log(err))
+
+
+app.get('/', async (req, res) => {
+    res.send('tasty food application run successfully')
+})
+
+
+
+app.listen(prot, () => { console.log(`TastyRide Run on ${prot}`) })
