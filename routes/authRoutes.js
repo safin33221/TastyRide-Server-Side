1d217a991f848a8bd6b1958ceff2ebdaf80892ec
safin33221 <saifn33221@gmail.com>
2025-03-17 23:40:50 +0600

    New Technology: Completd to get User data Using mongoose

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,12 +1,11 @@
 const express = require('express')
-const User = require('../model/authModel')
-const { registerUser } = require('../controller/authController')
+
+const { registerUser, getUsers } = require('../controller/authController')
 const router = express.Router()
 
 
 //get User 
-
-
+router.get('/users',getUsers)
 
 //Post New User
 router.post('/register', registerUser)
